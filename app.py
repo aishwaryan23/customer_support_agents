9cfed8f4292fb5d3caf131070c4e6e7a7f44649e
N Aishwarya <156683806+aishwaryan23@users.noreply.github.com>
2025-06-15 00:56:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class State(TypedDict):
 import os
 llm = ChatGroq(
     temperature=0,
-    export groq_api_key= os.getenv("GROQ_API_KEY"),
+    groq_api_key= os.getenv("GROQ_API_KEY"),
     model_name="llama-3.3-70b-versatile"
 )
 
